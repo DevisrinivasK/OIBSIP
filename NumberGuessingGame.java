ee9c75212106e5528ffe8935009f4db6506b3ab5
sujithachalla97 <168841679+sujithachalla97@users.noreply.github.com>
2024-07-03 12:37:56 +0530

    Update NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-
+import java.util.*;
 public class NumberGuessingGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
